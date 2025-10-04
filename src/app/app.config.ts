544aa6f315e861ddedd09d74c5ccfecf852d4b03
Menencia <menencia@gmail.com>
2025-10-04 16:40:15 +0200

    add primeng, tailwind & howler

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,33 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
+import { provideHttpClient } from '@angular/common/http';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
+import { providePrimeNG } from 'primeng/config';
 import { routes } from './app.routes';
+import MyPreset from './mypreset';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideRouter(
+      routes,
+      withInMemoryScrolling({ scrollPositionRestoration: 'enabled' }),
+    ),
+    provideHttpClient(),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      ripple: true,
+      theme: {
+        preset: MyPreset,
+        options: {
+          darkModeSelector: '.dark',
+        },
+      },
+    }),
+  ],
 };
---
+++ src/app/mypreset.ts
@@ -0,0 +1,23 @@
+//mypreset.ts
+import { definePreset } from '@primeuix/themes';
+import Aura from '@primeuix/themes/aura';
+
+const MyPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{stone.50}',
+      100: '{stone.100}',
+      200: '{stone.200}',
+      300: '{stone.300}',
+      400: '{stone.400}',
+      500: '{stone.500}',
+      600: '{stone.600}',
+      700: '{stone.700}',
+      800: '{stone.800}',
+      900: '{stone.900}',
+      950: '{stone.950}',
+    },
+  },
+});
+
+export default MyPreset;
